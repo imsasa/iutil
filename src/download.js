c3de4d4d05109c580452127a6a2745b65d2efba6
sasa <ataboy@live.cn>
2024-10-14 10:09:02 +0800

    Create download.js

---
+++ src/download.js
@@ -0,0 +1,10 @@
+export default function (content,opts){
+  content = content.constructor === Blob? content:[content];
+  const blob = new Blob(content, Object.assign({ type: "text/plain" }, opts);
+  const url = URL.createObjectURL(blob); // 创建一个 Blob URL
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "test.txt";
+  a.click();
+  URL.revokeObjectURL(url);
+}
