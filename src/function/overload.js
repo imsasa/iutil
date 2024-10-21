280f374b186a20d4a1d5901b5930c9c6b65bfedf
sasa <ataboy@live.cn>
2024-10-21 19:32:27 +0800

    Update overload.js

--- src/function/overload.js
+++ src/function/overload.js
@@ -1,19 +1,20 @@
-export default function createOverLoad(){
-    const callMap = new Map()
-    function overLoad(...args){
-        const key = args.map(arg => typeof arg).join(',')
-        // 根据剩余参数的类型 找到再map集合中对应需要处理的函数
-        const fn = callMap.get(key);
-       return !fn?throw new TypeError('no matching the function'):fn.apply(this, args);
+export default function createOverLoad() {
+  let defaultFn;
+  const callMap = new Map();
+  const overLoad = function (... args) {
+    const key = args.map(arg => typeof arg).join(',');
+    // 根据参数的类型找到map集合中对应需要处理的函数
+    const fn = callMap.get(key) || defaultFn;
+    return fn.apply(this, args);
+  };
+  // addImpl用来创建一个函数枚举的map
+  overLoad.addImpl = function (... args) {
+    const fn = args.pop();
+    if (typeof fn !== 'function') {
+      throw new TypeError('not a function');
     }
-
-    //addImpl 函数 用来创建一个函数枚举的map
-    overLoad.addImpl = function(...args){
-        const fn = args.pop()
-        if(typeof fn !== 'function') {
-            throw new TypeError('not a function')
-        }
-        callMap.set(args.join(','), fn)
-    }
-    return overLoad
+    defaultFn || (defaultFn = fn);
+    callMap.set(args.join(','), fn);
+  };
+  return overLoad;
 };
